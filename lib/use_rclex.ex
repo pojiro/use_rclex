68b6188bcd7f5598114644494b7de591451d3b7c
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-10-20 08:04:03 +0900

    Initial commit

---
+++ lib/use_rclex.ex
@@ -0,0 +1,18 @@
+defmodule UseRclex do
+  @moduledoc """
+  Documentation for `UseRclex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UseRclex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/use_rclex_test.exs
@@ -0,0 +1,8 @@
+defmodule UseRclexTest do
+  use ExUnit.Case
+  doctest UseRclex
+
+  test "greets the world" do
+    assert UseRclex.hello() == :world
+  end
+end
