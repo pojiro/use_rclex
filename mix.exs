aec25776b6757b090e18eda4d5997e827bda2b50
Ryota Kinukawa <kinukawa@tombo-works.com>
2022-10-22 07:38:33 +0900

    Add some codes to make using rclex

--- mix.exs
+++ mix.exs
@@ -21,8 +21,9 @@ defmodule UseRclex.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:rclex,
+       git: "https://github.com/rclex/rclex.git",
+       branch: "refactor-generate_messages_codes-pojiro"}
     ]
   end
 end
